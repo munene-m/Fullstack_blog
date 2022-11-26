e7a99cf525cddcb121b23dbcec1c3cfdb924cb77
munene-m <munenenjue@gmail.com>
2022-11-26 18:59:27 +0300

    initial commit

---
+++ backend/routes/auth.js
@@ -0,0 +1,38 @@
+const router = require('express').Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+
+//register
+router.post('/register', async (req,res) => {
+    try{
+        const salt = await bcrypt.genSalt(10)
+        const hashedPswd = await bcrypt.hash(req.body.password, salt)
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPswd
+        })
+        const user = await newUser.save()
+        res.status(200).json(user)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//login
+router.post('/login', async (req, res) => {
+    try{
+        const user = await User.findOne({username: req.body.username})
+        !user && res.status(500).json("Incorrect login details.")
+
+        const validate = await bcrypt.compare(req.body.password, user.password)
+        !validate && res.status(500).json("Incorrect login details.")
+
+        const { password, ...otherUserDetails} = user._doc
+        res.status(200).json(otherUserDetails)
+    }catch{
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router
